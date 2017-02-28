8cb5dee6443cac45a08ada87032d5da052a94f3d
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-02-28 02:31:26 +0100

    Ignore port compiler leftovers

--- c_src/fitness_entry_nif.d
+++
@@ -1,6 +0,0 @@
-c_src/fitness_entry_nif.o: c_src/fitness_entry_nif.c \
-  /Users/bzurkowski/.kerl/installations/17.5/erts-6.4/include/erl_nif.h \
-  /Users/bzurkowski/.kerl/installations/17.5/erts-6.4/include/erl_native_features_config.h \
-  /Users/bzurkowski/.kerl/installations/17.5/erts-6.4/include/erl_drv_nif.h \
-  /Users/bzurkowski/.kerl/installations/17.5/erts-6.4/include/erl_int_sizes_config.h \
-  /Users/bzurkowski/.kerl/installations/17.5/erts-6.4/include/erl_nif_api_funcs.h
